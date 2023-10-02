56c187a14201d6ad421ed90047b8e725ce7408b2
evgenysuleymanov <evgenysuleymanov@gmail.com>
2023-10-02 21:42:59 +0500

    Add solution for prime

--- homework_01/main.py
+++ homework_01/main.py
@@ -37,9 +37,9 @@ def filter_numbers(numbers_list, filter_type):
     >>> filter_numbers([2, 3, 4, 5], EVEN)
     <<< [2, 4]
     """
-    if filter_type == ODD:
+    if (filter_type == ODD):
         return [number for number in numbers_list if number % 2 != 0]
-    if filter_type == EVEN:
+    if (filter_type == EVEN):
         return [number for number in numbers_list if number % 2 == 0]
-    if filter_type == PRIME:
+    if (filter_type == PRIME):
         return [number for number in numbers_list if is_prime(number) is True]
